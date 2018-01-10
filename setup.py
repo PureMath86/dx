60971edc58454499acab9b97bb78b2b6f6f0cedb
Bryan Arnold <PureMath86@users.noreply.github.com>
2018-01-10 15:38:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import setup, Command, find_packages
 
 DISTNAME = 'dx'
 
 setup(name=DISTNAME,
 		version='0.1',
+                packages=find_packages(include=['dx', 'dx.*']),
 		description='DX Analytics',
 		author='Dr. Yves Hilpisch',
 		author_email='dx@tpq.io',
